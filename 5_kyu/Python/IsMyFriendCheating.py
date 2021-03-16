c017601510d15bb1c0d16c41982a7af2d2eb0717
wpspires <treyspires@gmail.com>
2021-03-16 11:29:11 -0500

    Clean up solution/comments for IsMyFriendCheating

--- 5_kyu/Python/IsMyFriendCheating.py
+++ 5_kyu/Python/IsMyFriendCheating.py
@@ -6,11 +6,17 @@ def remov_nb(n):
     # After comparing answers, n(n+1)/2 would have been better here
 
     for a in range(1, n+1):
-        b = (total - a)/ (a+1)
+        # total - a - b = a * b, solve for b
+        b = (total - a) / (a + 1)
         if b % 1 == 0 and b <= n:
             results.append((a, int(b)))
     return results
 
+
 results = remov_nb(26)
+print(results)
+results = remov_nb(100)
+print(results)
+results = remov_nb(101)
 print(results)
 
